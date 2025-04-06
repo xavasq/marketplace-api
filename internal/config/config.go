6b05755dbd3919f71d92742db591b94d2596780c
vazorian <vazoriandev@gmail.com>
2025-04-06 17:07:36 +0500

    Add config package and loadEnv function

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,31 @@
 package config
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DBName     string
+	DBUser     string
+	DBPassword string
+	DBHost     string
+	DBPort     string
+}
+
+func LoadEnv() *Config {
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Printf("ошибка при загрузке файла .env: %v", err)
+	}
+
+	return &Config{
+		DBName:     viper.GetString("DB_NAME"),
+		DBUser:     viper.GetString("DB_USER"),
+		DBPassword: viper.GetString("DB_PASSWORD"),
+		DBPort:     viper.GetString("DB_PORT"),
+		DBHost:     viper.GetString("DB_HOST"),
+	}
+}
