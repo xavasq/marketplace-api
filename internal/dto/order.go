985d187aae85c4fd737bf783ba5464930d007d57
xavasq <vazoriandev@gmail.com>
2025-04-17 17:27:43 +0500

    feat(dto): add initial validation for request structs

--- internal/dto/order.go
+++ internal/dto/order.go
@@ -1,14 +1,14 @@
 package dto
 
 type OrderRequestDTO struct {
-	ProductID int64 `json:"product_id"`
-	Quantity  int   `json:"quantity"`
+	ProductID int64 `json:"product_id" validate:"required,gt=0"`
+	Quantity  int   `json:"quantity" validate:"required,gt=0"`
 }
 
 type OrderResponseDTO struct {
-	ID        int64 `json:"ID"`
-	UserID    int64 `json:"UserID"`
-	ProductID int64 `json:"ProductID"`
+	ID        int64 `json:"id"`
+	UserID    int64 `json:"user_id"`
+	ProductID int64 `json:"product_id"`
 	Quantity  int   `json:"quantity"`
-	Price     int64 `json:"Price"`
+	Price     int64 `json:"price"`
 }
--- internal/dto/user.go
+++ internal/dto/user.go
@@ -1,9 +1,9 @@
 package dto
 
 type UserRequestDTO struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Role     string `json:"role"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8"`
+	Role     string `json:"role" validate:"required,oneof=admin user"`
 }
 
 type UserResponseDTO struct {
