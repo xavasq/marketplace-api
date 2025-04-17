91a2cc18999a277905391c4370a63e8b8cfc9574
xavasq <vazoriandev@gmail.com>
2025-04-17 20:26:56 +0500

    feat(dto): add initial validation for user and order structs

--- internal/dto/product.go
+++ internal/dto/product.go
@@ -1,7 +1,7 @@
 package dto
 
 type ProductResponseDTO struct {
-	ID       int64  `json:"id"`
+	ID       int64  `json:"idd"`
 	Name     string `json:"name"`
 	Price    int64  `json:"price"`
 	Quantity int    `json:"quantity"`
